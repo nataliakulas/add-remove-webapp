5faf75b46b451586756af832e3863414effcedb4
Natalia Kulas <nataliakulas@gmail.com>
2019-04-04 14:45:18 +0200

    Submit button with simple styling

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+import color from "../shared/colors";
+
+const Button = styled.button`
+  width: 100%;
+
+  font-family: "Lato", sans-serif;
+  color: ${color.white};
+  font-weight: bold;
+
+  border: 1px solid ${color.blue};
+  background-color: ${color.blue};
+  border-radius: 22px;
+
+  margin: 20px auto;
+  padding: 10px 20px;
+  cursor: pointer;
+
+  &:hover {
+    border-color: ${color.lapis};
+    background-color: ${color.lapis};
+  }
+`;
+
+export default Button;
