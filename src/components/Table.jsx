7464fcfddad78aa421d6e03c8e9df37051f952e2
Natalia Kulas <nataliakulas@gmail.com>
2019-04-05 12:58:17 +0200

    Sorting implemented

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Row, Col } from "react-grid-system";
 import styled from "styled-components";
 
@@ -8,57 +8,134 @@ import { PanelMixin, BoldBlueMixin } from "../shared/mixins";
 import Button, { RoundButton } from "./Button";
 import { P } from "./Fonts";
 
-const UserTable = ({ users, onRemove, onRemoveAll }) => (
-  <Table>
-    <Header>
-      <Row>
-        <Col sm={3} lg={4}>
-          <P>Nickname</P>
-        </Col>
-        <Col sm={3}>
-          <P>E-mail</P>
-        </Col>
-        <Col sm={3}>
-          <P>IP address</P>
-        </Col>
-        <Col sm={3} lg={2}>
-          {users.length > 0 && (
-            <Button margin="0" type="button" onClick={onRemoveAll}>
-              Remove All
-            </Button>
-          )}
-        </Col>
-      </Row>
-    </Header>
-    <Body>
-      {users.map(({ timestamp, nickname, email, ip }) => (
-        <Row key={timestamp}>
+const sort_type = {
+  ASC: "ASC",
+  DESC: "DESC"
+};
+
+const UserTable = ({ users, onRemove, onRemoveAll }) => {
+  const initialState = { field: "", sort: "" };
+
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case sort_type.ASC:
+        return {
+          field: action.payload,
+          sort: action.type
+        };
+      case sort_type.DESC:
+        return {
+          field: action.payload,
+          sort: action.type
+        };
+      default:
+        return state;
+    }
+  }, initialState);
+
+  const handleSort = (field, sort) => dispatch({ type: sort, payload: field });
+
+  const disabled = users.length < 2;
+
+  const sorted_users = users.sort((prev, next, ...user) => {
+    if (state.sort === sort_type.ASC) {
+      return prev[state.field].localeCompare(next[state.field]);
+    } else if (state.sort === sort_type.DESC) {
+      return next[state.field].localeCompare(prev[state.field]);
+    }
+    return user;
+  });
+
+  return (
+    <Table>
+      <Header>
+        <Row>
           <Col sm={3} lg={4}>
-            <P>{nickname}</P>
+            <Wrapper>
+              <P>Nickname</P>
+              <SortButtons
+                field="nickname"
+                disabled={disabled}
+                onSort={handleSort}
+              />
+            </Wrapper>
           </Col>
           <Col sm={3}>
-            <P>{email}</P>
+            <Wrapper>
+              <P>E-mail</P>
+              <SortButtons
+                field="email"
+                disabled={disabled}
+                onSort={handleSort}
+              />
+            </Wrapper>
           </Col>
           <Col sm={3}>
-            <P>{ip}</P>
+            <Wrapper>
+              <P>IP address</P>
+              <SortButtons field="ip" disabled={disabled} onSort={handleSort} />
+            </Wrapper>
           </Col>
-          <Col sm={1} offset={{ sm: 1 }}>
-            <RoundButton
-              outlined
-              type="button"
-              margin="10px auto"
-              onClick={() => onRemove({ nickname, timestamp })}
-            >
-              <span role="img" aria-label="remove">
-                &#10060;
-              </span>
-            </RoundButton>
+          <Col sm={3} lg={2}>
+            {users.length > 0 && (
+              <Button margin="0" type="button" onClick={onRemoveAll}>
+                Remove All
+              </Button>
+            )}
           </Col>
         </Row>
-      ))}
-      {users.length === 0 && <Placeholder>Add some users!</Placeholder>}
-    </Body>
-  </Table>
+      </Header>
+      <Body>
+        {sorted_users.map(({ timestamp, nickname, email, ip }) => (
+          <Row key={timestamp}>
+            <Col sm={3} lg={4}>
+              <P>{nickname}</P>
+            </Col>
+            <Col sm={3}>
+              <P>{email}</P>
+            </Col>
+            <Col sm={3}>
+              <P>{ip}</P>
+            </Col>
+            <Col sm={1} offset={{ sm: 1 }}>
+              <RoundButton
+                outlined
+                type="button"
+                margin="10px auto"
+                onClick={() => onRemove({ nickname, timestamp })}
+              >
+                <span role="img" aria-label="remove">
+                  &#10060;
+                </span>
+              </RoundButton>
+            </Col>
+          </Row>
+        ))}
+        {users.length === 0 && <Placeholder>Add some users!</Placeholder>}
+      </Body>
+    </Table>
+  );
+};
+
+const SortButtons = ({ field, disabled, onSort }) => (
+  <div>
+    <RoundButton
+      type="button"
+      margin="0 5px"
+      disabled={disabled}
+      onClick={() => onSort(field, sort_type.ASC)}
+    >
+      &#x25B2;
+    </RoundButton>
+    <RoundButton
+      type="button"
+      margin="0 5px"
+      disabled={disabled}
+      onClick={() => onSort(field, sort_type.DESC)}
+    >
+      &#x25BC;
+    </RoundButton>
+  </div>
 );
 
 export default UserTable;
@@ -86,4 +163,11 @@ const Header = styled.div`
 
 const Body = styled.div`
   padding-top: 20px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
